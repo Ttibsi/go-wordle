21a245be9872efd981b8f8bb66b5684f3b8a852a
ttibsi <ashisbitt@icloud.com>
2022-06-18 15:25:16 +0100

    View and initial import

---
+++ pkg/logic.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/charmbracelet/bubbles/textinput"
+)
+
+func generateAnswer() []string {
+	// Is there seriously no easier way to readlines a file?
+	var wordlist []string
+	file, err := os.Open("wordlist.txt")
+	if err != nil {
+		log.Fatal()
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		wordlist = append(wordlist, scanner.Text())
+	}
+
+	rand.Seed(time.Now().UnixNano())
+    answer := wordlist[rand.Intn(len(wordlist))]
+    return strings.Split(answer, "")
+}
---
+++ pkg/model.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/bubbles/textinput"
+)
+
+type model struct {
+    guesses [6][5]string // Words already guessed by user
+	answer  []string   // seed word generated from txtfile as char array
+	textInput textinput.Model
+}
+
+func initialModel() model {
+	return model{
+		guesses: [6][5]string{},
+		answer:  generateAnswer(),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+        case tea.KeyMsg:
+            switch msg.String() {
+            case "ctrl+c", "q":
+                return m, tea.Quit
+            case "enter":
+                var newGuess = getNewGuess()
+                // Take text entry from elsewhere and trigger a function to check word
+                // append as an array
+            }
+	}
+
+    return m, nil
+}
+
+// User interface stored as a string
+func (m model) View() string {
+    // The header
+    s := "Go-Wordle\n\n"
+
+    for _, item := range m.guesses {
+        // Render row
+        var row string
+        for _, letter  := range item {
+            row += letter + " "
+           // Still need to figure out cards
+        }
+        s += fmt.Sprintf(row)
+
+        //s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+    }
+
+    // The footer
+    s += "\nq or ctrl+c to quit.\n"
+
+    // Send the UI for rendering
+    return s
+}
+
