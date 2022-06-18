b6361af4bb7d44fa42d2076de0b2003052dc4740
ttibsi <ashisbitt@icloud.com>
2022-06-18 15:41:27 +0100

    Packaging changing

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,11 @@ import (
 	"os"
 
     tea "github.com/charmbracelet/bubbletea"
-    "github.com/Ttibsi/go-wordle/tree/tui/pkg"
+    "github.com/Ttibsi/go-wordle/tree/tui/pkg/tui"
 )
 
 func main() {
-    p := tea.NewProgram(pkg.initialModel())
+    p := tea.NewProgram(tui.initialModel())
         if err := p.Start(); err != nil {
             fmt.Printf("Alas, there's been an error: %v", err)
             os.Exit(1)
